98b5b3eba326203fad5ba28d168449724f34c0c4
Tavares-cmda <henrique.tavares.pedro@escola.pr.gov.br>
2026-03-19 08:24:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function abrirLogin(tipo){
   document.getElementById("home").classList.add("hidden");
   document.getElementById("login").classList.remove("hidden");
-
   document.getElementById("tituloLogin").innerText = "Login - " + tipo;
 }
 
@@ -11,6 +10,10 @@ function voltar(){
 }
 
 function entrar(){
+  const btn = document.getElementById("btnEntrar");
+
+  if(btn.disabled) return;
+
   document.getElementById("login").classList.add("hidden");
   document.getElementById("menu").classList.remove("hidden");
 }
@@ -18,4 +21,24 @@ function entrar(){
 function sair(){
   document.getElementById("menu").classList.add("hidden");
   document.getElementById("home").classList.remove("hidden");
+}
+
+/* VALIDAÇÃO */
+function validarCampos(){
+  const cpf = document.getElementById("cpf").value.trim();
+  const celular = document.getElementById("celular").value.trim();
+  const btn = document.getElementById("btnEntrar");
+
+  if(cpf !== "" && celular !== ""){
+    btn.disabled = false;
+    btn.classList.add("ativo");
+  }else{
+    btn.disabled = true;
+    btn.classList.remove("ativo");
+  }
+}
+
+/* botão de código (opcional) */
+function enviarCodigo(){
+  alert("Código enviado!");
 }
